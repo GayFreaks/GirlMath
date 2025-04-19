e2d92a806737262fcb350e8561282affa7dcc673
cornelius <macrosedk@gmail.com>
2025-04-19 15:33:40 +0200

    Move to a headless infra and implement deg2rad and rad2deg

---
+++ GirlMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GirlMath;
+
+public class GirlMath
+{
+    public float deg2rad(float degree)
+    {
+        return (float)degree * (Math.PI / 180);
+    }
+
+    public float rad2deg(float radian)
+    {
+        return (float)radian * (180 / Math.PI);
+    }
+
+    public double deg2rad(double degree)
+    {
+        return (double)degree * (Math.PI / 180);
+    }
+
+    public double rad2deg(double radian)
+    {
+        return (double)radian * (180 / Math.PI);
+    }
+}
